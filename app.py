ae3ed726be1d486e4f7c327c4e55034966d73d25
Mochamad Abdul Azis <109265521+mochabdulazis@users.noreply.github.com>
2025-06-02 00:04:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import json
 @st.cache_resource
 def load_model():
     model_path = "Syetsuki/hoax-detector"
-    tokenizer = BertTokenizerFast.from_pretrained(f"{model_path}/tokenizer")
+    tokenizer = BertTokenizerFast.from_pretrained(f"{model_path}")
     model = BertForSequenceClassification.from_pretrained("indobenchmark/indobert-base-p1", num_labels=2)
-    model.load_state_dict(torch.load(f"{model_path}/hoax_detector_model.pth", map_location=torch.device("cpu")))
+    model.load_state_dict(torch.load(f"{model_path}/pytorch_model.bin", map_location=torch.device("cpu")))
     model.eval()
     return tokenizer, model
 
